209530a4b3a6e5076240c42fb8af9dac9263e156
黄晓东 <1357805398@qq.com>
2024-05-08 15:23:39 +0800

    fix:  "Dynamic require of "os" is not supported"
    [#1921](https://github.com/evanw/esbuild/issues/1921)

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,13 @@
 import type { Options } from 'tsup'
 
 export default <Options>{
+  banner: ({ format }) => {
+    if (format === 'esm') {
+      return {
+        js: `import {createRequire as __createRequire} from 'module';var require=__createRequire(import\.meta.url);`,
+      };
+    }
+  },
   entryPoints: [
     'src/*.ts',
   ],
